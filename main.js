c8c8b84e2b333e918eefeefe1083d69923594c51
Trealyum <64218769+Trealyum@users.noreply.github.com>
2020-05-13 22:40:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const { Client, Collection } = require('discord.js');
+const { TOKEN, PREFIX } = require('./config');
+
+const client = new Client();
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+console.log(commandFiles);
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+  console.log(`Commande chargée: ${command.name}`);
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+  const args = message.content.slice(PREFIX.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) return;
+  client.commands.get(command).execute(client, message, args);
+});
+
+client.login(TOKEN);
