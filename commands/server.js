5dc54777b256f0c4d415431059a559a8cde61775
Trealyum <64218769+Trealyum@users.noreply.github.com>
2020-05-13 19:07:09 +0200

    Delete server.js

--- commands/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'server',
-  description: 'Renvoie les informations du server',
-  execute(message, args) {
-    message.channel.send(`Je suis sur le serveur ${message.guild.name}.`);
-  }
-}
