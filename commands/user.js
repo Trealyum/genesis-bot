5e2abdea74428ce8fa194f0d54259577b78a5f9a
Trealyum <64218769+Trealyum@users.noreply.github.com>
2020-05-13 19:07:20 +0200

    Delete user.js

--- commands/user.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'user',
-  description: 'Renvoie les informations de l\'utilisateur de la commande',
-  execute(message, args) {
-    channel.send(`Je suis l'utilisateur ${message.author.tag}.`);
-  }
-}
