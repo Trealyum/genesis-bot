74738f9286accded5228465fad7fe1773451896c
Trealyum <64218769+Trealyum@users.noreply.github.com>
2020-05-14 15:06:28 +0200

    Delete userinfo.js

--- commands/userinfo.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'userinfo',
-  description: 'Renvoie les informations d\'un utilisateur mentionné',
-  execute(client, message, args) {
-    const user_mention = message.mentions.users.first();
-    message.channel.send(`Voici le tag de la personne mentionné: ${user_mention.tag}`);
-  }
-}
