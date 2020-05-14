fdb252c90c5e8d89d09f1a91a0e5a12c87bc86a6
Trealyum <64218769+Trealyum@users.noreply.github.com>
2020-05-14 15:06:43 +0200

    Delete ping.js

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: 'Renvoie le mot pong à l\'utilisateur de la commande',
-  execute(client, message, args) {
-  message.channel.send("Pong!");
-  }
-}
