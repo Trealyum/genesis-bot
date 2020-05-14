6cc68cdfe35177b51456cd7c8ff95018c1decdac
Trealyum <64218769+Trealyum@users.noreply.github.com>
2020-05-14 09:44:47 +0200

    Add files via upload

---
+++ commands/add.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'add',
+  description: 'Ahouter un rôle!',
+  execute(client, message, args) {
+    let role = message.guild.roles.cache.find(r => r.name === args.toString());
+    if (role) {
+      if (message.member.roles.cache.has(role.id)) return message.channel.send("Vous avez déjà ce rôle! Essayez à nouveau!");
+      if (role.permissions.has('KICK_MEMBERS')) return message.channel.send("Vous ne pouvez pas avoir ce rôle!");
+
+      message.member.roles.add(role)
+        .then(m => message.channel.send(`Vous possédez maintenant le rôle ${role}.`))
+        .catch(e => console.log(e));
+    } else {
+      message.channel.send("Le rôle n'existe pas!");
+    }
+  }
+}
