6a6bf40b667521f6347cb5ae1507f3ac3a416d82
Trealyum <64218769+Trealyum@users.noreply.github.com>
2020-05-14 15:05:57 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,42 @@
-const fs = require("fs");
 const { Client, Collection } = require('discord.js');
 const { TOKEN, PREFIX } = require('./config');
+const { readdirSync } = require("fs");
 
 const client = new Client();
 client.commands = new Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-console.log(commandFiles);
+const loadCommands = (dir = "./commands/") => {
+  readdirSync(dir).forEach(dirs => {
+    const commands = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"));
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-  console.log(`Commande chargée: ${command.name}`);
-}
+    for (const file of commands) {
+      const getFileName = require(`${dir}/${dirs}/${file}`);
+      client.commands.set(getFileName.help.name, getFileName);
+      console.log(`Commande chargée: ${getFileName.help.name}`);
+    };
+  });
+};
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+loadCommands();
 
 client.on('message', message => {
   if (!message.content.startsWith(PREFIX) || message.author.bot) return;
   const args = message.content.slice(PREFIX.length).split(/ +/);
-  const command = args.shift().toLowerCase();
+  const commandName = args.shift().toLowerCase();
 
-  if (!client.commands.has(command)) return;
-  client.commands.get(command).execute(client, message, args);
+  if (!client.commands.has(commandName)) return;
+  const command = client.commands.get(commandName);
+
+  if (command.help.args && !args.length) {
+    let noArgsReply = `Il nous faut des arguments pour cette commande, ${message.author}!`;
+
+    if (command.help.usage) noArgsReply += `\nVoici comment utiliser la commande: \`${PREFIX}${command.help.name} ${command.help.usage}\``
+
+    return message.channel.send(noArgsReply);
+  }
+
+  command.run(client, message, args);
 });
 
+client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
 client.login(TOKEN);
